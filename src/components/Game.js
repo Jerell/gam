eef499ba8dd9e720310d219cec458954fe16d553
Jerell James <jerell1998@gmail.com>
2020-10-10 19:41:54 +0100

    mouse sockets

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,33 +4,41 @@ import p5 from "p5";
 
 export default function Game({ socket }) {
   const Sketch = (p5) => {
-    const settings = {
-      bgCol: 0,
-    };
+    // Sending data to the socket
+    function sendmouse(x, y, pX, pY) {
+      const data = {
+        x: x,
+        y: y,
+        px: pX,
+        py: pY,
+      };
+      socket.emit("mouse", data);
+    }
+
+    function receiveMouse(data) {
+      p5.fill(data.color);
+      p5.ellipse(data.x, data.y, 50, 50);
+      // name
+      let textWidth = p5.textWidth(data.name);
+      p5.fill(0);
+      p5.text(data.name, data.x - textWidth / 2, data.y);
+    }
+
+    socket.on("mouse", (data) => {
+      receiveMouse(data);
+    });
 
     p5.setup = () => {
       const gameContainer = document.getElementById("gameContainer");
       p5.createCanvas(gameContainer.offsetWidth, gameContainer.offsetHeight);
       p5.background(0);
+      p5.noStroke();
     };
 
-    // p5.mouseDragged = () => {
-    //   const { mouseX, mouseY } = p5;
-    //   // Make a little object with mouseX and mouseY
-    //   let data = {
-    //     x: mouseX,
-    //     y: mouseY,
-    //   };
-    //   // Send that object to the socket
-    //   socket.emit("mouse", data);
-    // };
-
-    // p5.draw = () => {
-    //   p5.background(bgCol);
-    //   // p5.ellipse(p5.width / 2, p5.height / 2, radius, radius);
-    // };
-
-    // p5.mouseDragged
+    p5.mouseDragged = () => {
+      const { mouseX: x, mouseY: y, pmouseX: pX, pmouseY: pY } = p5;
+      sendmouse(x, y, pX, pY);
+    };
   };
 
   let gRef = React.createRef();
