83a9e0b076220d1feed1138a22225a7793cb2fd3
Jerell James <jerell1998@gmail.com>
2020-10-10 20:22:00 +0100

    Updates

---
+++ precache-manifest.1bbc15dbcc18f78c1e829d5dd44761e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "226cac6e589967eef10481bd1f5ff5f6",
+    "url": "/gam/index.html"
+  },
+  {
+    "revision": "418743476f40c363690c",
+    "url": "/gam/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1dd57a192d90708812ee",
+    "url": "/gam/static/js/2.31bceacf.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/gam/static/js/2.31bceacf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418743476f40c363690c",
+    "url": "/gam/static/js/main.6e1e4576.chunk.js"
+  },
+  {
+    "revision": "c3bd52dde418f3c43bc6",
+    "url": "/gam/static/js/runtime-main.abca2f20.js"
+  }
+]);
