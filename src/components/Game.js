8155164229ee1d4ac39169ed500dfec386fb42f1
Jerell James <jerell1998@gmail.com>
2020-10-11 13:24:12 +0100

    start of player class

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,7 +2,40 @@ import React, { useEffect } from "react";
 import { Box } from "@material-ui/core";
 import p5 from "p5";
 
+class Character {
+  constructor(drawingContext, x, y, color, size, name) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.name = name;
+    this.color = color;
+    this.canvas = drawingContext;
+    this.dc = drawingContext.createGraphics(size, size);
+    this.dc.clear();
+  }
+  render() {
+    this.dc.clear();
+    this.canvas.image(this.x, this.y, this.size, this.size);
+  }
+}
+
+class Player extends Character {
+  constructor(drawingContext, x, y, color, name) {
+    super(...arguments, 50);
+  }
+  draw() {
+    this.dc.fill(this.color);
+    this.dc.square(0, 0, this.size);
+    this.dc.text();
+    // p5.text(data.name, data.x - textWidth / 2, data.y);
+    this.render();
+  }
+}
+
 export default function Game({ socket }) {
+  const players = {};
+  function addPlayer(name) {}
+
   const Sketch = (p5) => {
     // Sending data to the socket
     function sendmouse(x, y, pX, pY) {
@@ -17,11 +50,11 @@ export default function Game({ socket }) {
 
     function receiveMouse(data) {
       p5.fill(data.color);
-      p5.ellipse(data.x, data.y, 50, 50);
+      p5.ellipse(data.pos.x, data.pos.y, 50, 50);
       // name
       let textWidth = p5.textWidth(data.name);
       p5.fill(0);
-      p5.text(data.name, data.x - textWidth / 2, data.y);
+      p5.text(data.name, data.pos.x - textWidth / 2, data.pos.y);
     }
 
     socket.on("mouse", (data) => {
@@ -35,7 +68,7 @@ export default function Game({ socket }) {
       p5.noStroke();
     };
 
-    p5.mouseDragged = () => {
+    p5.mouseMoved = () => {
       const { mouseX: x, mouseY: y, pmouseX: pX, pmouseY: pY } = p5;
       sendmouse(x, y, pX, pY);
     };
